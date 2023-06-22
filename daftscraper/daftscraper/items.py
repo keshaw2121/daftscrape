b23c5043d15019d535139a626c823b7e9ff33f50
keshaw2121 <keshaw2121@gmail.com>
2023-06-22 17:03:43 +0100

    Created items.py file with all the data fields as item and cleaned the daftspider

--- daftscraper/daftscraper/items.py
+++ daftscraper/daftscraper/items.py
@@ -7,6 +7,15 @@ import scrapy
 
 
 class DaftscraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    DaftID = scrapy.Field()
+    Address = scrapy.Field()
+    Property_Type = scrapy.Field()
+    Price = scrapy.Field()
+    Beds = scrapy.Field()
+    Baths = scrapy.Field()
+    Available_From = scrapy.Field()
+    Furnished = scrapy.Field()
+    Lease = scrapy.Field()
+    Description = scrapy.Field()
+    Date_Listed = scrapy.Field()
+    Views = scrapy.Field()
--- daftscraper/daftscraper/spiders/daftspider.py
+++ daftscraper/daftscraper/spiders/daftspider.py
@@ -1,34 +1,49 @@
 import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
+#https://www.daft.ie/property-for-rent/
 
-class DaftspiderSpider(scrapy.Spider):
+class DaftspiderSpider(CrawlSpider):
     name = "daftspider"
     allowed_domains = ["www.daft.ie"]
-    start_urls = ["https://www.daft.ie/for-rent/house-38a-beechwood-avenue-lower-dublin-6-ranelagh-dublin-6/5294088"]
+    start_urls = ["https://www.daft.ie/"]
 
-    def parse(self, response):
-        daftid = response.css('.DaftIDText__StyledDaftIDParagraph-vbn7aa-0::text').extract()[1]
-        address = response.css('[data-testid="address"]::text').extract()[0]
-        price = response.css('[data-testid="price"] h2::text').extract()[0]
-        beds = response.css('[data-testid="beds"]::text').extract()[0]
-        baths = response.css('[data-testid="baths"]::text').extract()[0]
-        availablefrom = response.css('[data-testid="overview"] li::text').getall()[5]
-        furnished = response.css('[data-testid="overview"] li::text').getall()[7]
-        lease = response.css('[data-testid="overview"] li::text').getall()[9]
-        description = response.css('[data-testid="description"]::text').extract()
-        datelisted = response.css('[data-testid="statistics"] p::text').extract()[0]
-        views = response.css('[data-testid="statistics"] p::text').extract()[2]
+    rules = (
+        Rule(LinkExtractor(allow='property-for-rent'), follow = True),
+        Rule(LinkExtractor(allow='for-rent'), callback='parse_item', follow = True)
+    )
 
-        yield {
-            'DaftID' : daftid,
-            'Address' : address,
-            'Price' : price,
-            'Beds' : beds,
-            'Baths' : baths,
-            'Available_From' : availablefrom,
-            'Furnished' : furnished,
-            'Lease' : lease,
-            'Description' : description,
-            'Date' : datelisted,
-            'Views' : views
-        }
+
+
+
+    def parse_item(self, response):
+            daftid = response.css('.DaftIDText__StyledDaftIDParagraph-vbn7aa-0::text').extract()[1]
+            url = response.url
+            address = response.css('[data-testid="address"]::text').get()
+            propertytype = response.css('[data-testid="property-type"]::text').get(),
+            price = response.css('[data-testid="price"] h2::text').getall()[0]
+            beds = response.css('[data-testid="beds"]::text').get()
+            baths = response.css('[data-testid="baths"]::text').get()
+            availablefrom = response.css('[data-testid="overview"] li::text').getall()[5]
+            furnished = response.css('[data-testid="overview"] li::text').getall()[7]
+            lease = response.css('[data-testid="overview"] li::text').getall()[9]
+            description = response.css('[data-testid="description"]::text').get()
+            datelisted = response.css('[data-testid="statistics"] p::text').get()
+            views = response.css('[data-testid="statistics"] p::text').getall[2]
+
+            yield {
+                'DaftID' : daftid,
+                'Url' : url,
+                'Address' : address,
+                'Property_Type' : propertytype,
+                'Price' : price,
+                'Beds' : beds,
+                'Baths' : baths,
+                'Available_From' : availablefrom,
+                'Furnished' : furnished,
+                'Lease' : lease,
+                'Description' : description,
+                'Date' : datelisted,
+                'Views' : views
+            }
